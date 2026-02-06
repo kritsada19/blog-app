76a38fe187eb566b60043bfae30800651fa8a097
Kritsada Mooljam <ninep9p2006@gmail.com>
2026-02-06 19:24:40 +0700

    feat: add framer-motion dependency and implement responsive navigation links; create Contact page with contact information

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -30,19 +30,35 @@ const techStack = [
   { icon: FaGithub, color: "text-gray-100" },
 ];
 
+function getAge(birthDate: string | Date): number {
+  const today = new Date();
+  const birth = new Date(birthDate);
+
+  let age = today.getFullYear() - birth.getFullYear();
+  const m = today.getMonth() - birth.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+    age--;
+  }
+
+  return age;
+}
+
+
 export default function AboutPage() {
   return (
     <section className="min-h-screen bg-black flex items-center justify-center px-6">
       <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-        {/* LEFT */}
         <div>
-          <h1 className="text-3xl font-bold text-emerald-500 mb-6">
+          <h1 className="text-xl sm:text-3xl font-bold text-emerald-500 mb-6">
             I am Kritsada, a Full-Stack Developer.
           </h1>
 
           <div className="space-y-4 text-sm">
-            <Info label="EMAIL" value="ninep9p2006@gmail.com" />
-            <Info label="PHONE" value="(+66) 88 557 8023" />
+            <Info label="NAME" value="Kritsada Mooljam (P)" />
+            <Info label="ROLE" value="Full-Stack Developer (Student)" />
+            <Info label="AGE" value={`${getAge("2006-09-19")}`} />
+            <Info label="LOCATION" value="Chiang Rai, Thailand" />
 
             <div>
               <p className="font-semibold text-gray-400 mb-2">TECH STACK</p>
@@ -55,7 +71,17 @@ export default function AboutPage() {
           </div>
 
           <Link
-            href="https://github.com/kritsasa"
+            href="/contact"
+            className="mt-8 inline-flex items-center gap-2 rounded-lg
+                       border border-emerald-500 px-4 py-3
+                       font-semibold text-emerald-500
+                       hover:bg-emerald-500 hover:text-black
+                       transition mr-4"
+          >
+            Contact Me
+          </Link>
+          <Link
+            href="https://github.com/kritsada19"
             target="_blank"
             className="mt-8 inline-flex items-center gap-2 rounded-lg
                        border border-emerald-500 px-4 py-3
@@ -63,20 +89,19 @@ export default function AboutPage() {
                        hover:bg-emerald-500 hover:text-black
                        transition"
           >
-            Go to GitHub <FaGithub size={20} />
+            Github <FaGithub />
           </Link>
         </div>
 
-        {/* RIGHT */}
         <div className="relative flex justify-center">
-          <div className="absolute w-72 h-72 md:w-96 md:h-96 rounded-full bg-emerald-500 -right-6 -top-6" />
+          <div className="absolute w-60 h-60 sm:w-96 sm:h-96 rounded-full bg-emerald-500 right-6 -top-6 sm:-right-6" />
 
           <Image
             src="/images/profile.jpg"
             alt="Kritsada profile"
             width={384}
             height={384}
-            className="relative w-72 h-72 md:w-96 md:h-96 rounded-full object-cover"
+            className="relative w-60 h-60 sm:w-96 sm:h-96 rounded-full object-cover"
             priority
           />
         </div>
